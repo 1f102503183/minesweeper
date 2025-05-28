bb42bf12d273fafd27a0d8cee72ea04a81dceece
1f102503183 <s1f102503183@iniad.org>
2025-05-28 22:11:46 +0900

    ゼロの周りを開けられるようにできた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,17 @@
 import { useState } from 'react';
 import styles from './page.module.css';
 
+const directions = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+] as const;
+
 //難易度に応じたサイズを返す
 // const dif = (def: string): number => {
 //   const size = 0;
@@ -28,65 +39,90 @@ const createBoard = (hight: number, width: number): number[][] => {
 
 //bommapを数字の書かれたものに書き換える
 const serch = (bomMap: number[][]): number[][] => {
-  const directions = [
-    [-1, -1],
-    [-1, 0],
-    [-1, 1],
-    [0, -1],
-    [0, 1],
-    [1, -1],
-    [1, 0],
-    [1, 1],
-  ];
-  const incNumber: number[][] = [];
+  const incNumber: number[][] = bomMap.map((row) => row.map(() => -1));
   for (let y = 0; y < bomMap.length; y++) {
-    const row = [];
     for (let x = 0; x < bomMap[y].length; x++) {
-      row.push(0);
-      for (let i = 0; i < directions.length; i++) {
-        const dy = y + directions[i][0];
-        const dx = x + directions[i][1];
-        if (
-          0 <= dy &&
-          dy < bomMap.length &&
-          0 < dx &&
-          x < bomMap[y].length &&
-          bomMap[dy][dx] === 1
-        ) {
-          row[x] += 1;
+      if (bomMap[y][x] === 1) {
+        incNumber[y][x] = 11;
+      } else {
+        incNumber[y][x] = 0;
+        for (let i = 0; i < directions.length; i++) {
+          const dy = y + directions[i][0];
+          const dx = x + directions[i][1];
+          if (
+            0 <= dy &&
+            dy < bomMap.length &&
+            0 <= dx &&
+            x < bomMap[y].length &&
+            bomMap[dy][dx] === 1
+          ) {
+            incNumber[y][x] += 1;
+          }
         }
       }
     }
-    incNumber.push(row);
   }
+  console.log(bomMap.length);
+  console.log(incNumber);
   return incNumber;
 };
 
 //二つのボードを統合
-const integBoard = (cBoard: number[][], bomMap: number[][]): number[][] => {
-  const incNum = serch(bomMap);
-  const board: number[][] = [];
+const integBoard = (cBoard: number[][], incNum: number[][]): number[][] => {
+  const board: number[][] = cBoard.map((row) => row.map(() => -1));
   for (let y = 0; y < cBoard.length; y++) {
-    const row = [];
     for (let x = 0; x < cBoard[y].length; x++) {
-      row.push(cBoard[y][x] === 0 ? -1 : bomMap[y][x] === 1 ? 11 : incNum[y][x]);
+      if (cBoard[y][x] === 1) {
+        openZero(cBoard, incNum, board, y, x);
+        board[y][x] = incNum[y][x];
+      }
     }
-    board.push(row);
+  }
+  for (let y = 0; y < cBoard.length; y++) {
+    for (let x = 0; x < cBoard[y].length; x++) {
+      if (board[y][x] !== -1) {
+        board[y][x] = incNum[y][x];
+      }
+    }
   }
   return board;
 };
 
+//ゼロの所をあける再起関数
+const openZero = (
+  cBoard: number[][],
+  incNum: number[][],
+  bord: number[][],
+  y: number,
+  x: number,
+) => {
+  if (incNum[y][x] === 0 && bord[y][x] === -1) {
+    bord[y][x] = 0;
+    for (const [dyOffset, dxOffset] of directions) {
+      const dy = y + dyOffset;
+      const dx = x + dxOffset;
+      if (0 <= dy && dy < incNum.length && 0 <= dx && dx < incNum[dy].length) {
+        if (incNum[dy][dx] === 0) {
+          openZero(cBoard, incNum, bord, dy, dx);
+        } else {
+          bord[dy][dx] = 0;
+        }
+      }
+    }
+  }
+};
+
 //ボムを配置
-const putBom = (Map: number[][]): number[][] => {
+const putBom = (Map: number[][], y: number, x: number): number[][] => {
   const a = Math.floor(Math.random() * 9);
   const b = Math.floor(Math.random() * 9);
   if (Map.flat().filter((i) => i === 1).length === 10) {
     return Map;
   }
-  if (Map[a][b] !== 1) {
+  if (Map[a][b] !== 1 && a !== y && b !== x) {
     Map[a][b] = 1;
   }
-  return putBom(Map);
+  return putBom(Map, y, x);
 };
 
 export default function Home() {
@@ -97,21 +133,21 @@ export default function Home() {
 
   const clickBoard: number[][] = structuredClone(userInput);
 
-  const calcBoard = integBoard(clickBoard, bomMap);
+  const calcBoard = integBoard(clickBoard, serch(bomMap));
   const width = calcBoard.length;
 
   const clickHandler = (x: number, y: number) => {
-    if (first === true) {
-      setBom(putBom(userInput));
+    if (first) {
+      setBom(putBom(userInput, y, x));
       setfirst(false);
     }
     clickBoard[y][x] = 1;
     setuser(clickBoard);
-    console.log(clickBoard);
   };
 
   return (
     <div className={styles.container}>
+      <div className={styles.face} style={{ backgroundPosition: `${-45 * 1}px` }} />
       <div className={styles.back} style={{ width: `${30 * width}px` }}>
         {calcBoard.map((row, y) =>
           row.map((state, x) => (
